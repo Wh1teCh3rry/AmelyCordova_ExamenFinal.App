7b60444de0e8d9aba8cbdd6f23856608dfe2e1fe
Amely Cordova <amely.cordova@udla.edu.ec>
2023-02-01 08:31:55 -0500

    Agregar archivos de proyecto.

---
+++ ACData/AC_CocktailDatabase.cs
@@ -0,0 +1,39 @@
+﻿using SQLite;
+using AmelyCordova_ExamenFinal.ACModels
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmelyCordova_ExamenFinal.ACData
+{
+    public class AC_CockatailDatabase
+    {
+        string _dbPath;
+        private SQLiteConnection conn;
+        public AC_CockatailDatabase(string DatabasePath)
+        {
+            _dbPath = DatabasePath;
+        }
+        private void Init()
+        {
+            if (conn != null)
+                return;
+            conn = new SQLiteConnection(_dbPath);
+            conn.CreateTable<AC_Cocktail>();
+        }
+        public int AddNewElement(AC_Cocktail cocktail)
+        {
+            Init();
+            int result = conn.Insert(cocktail);
+            return result;
+        }
+        public List<AC_Cocktail> GetAllElements()
+        {
+            Init();
+            List<AC_Cocktail> cocktails = conn.Table<C_Cocktail>().ToList();
+            return cocktails;
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AmelyCordova_ExamenFinal;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
